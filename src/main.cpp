9bbcc53e6597aeeef1495ad308f9decd4b5abdc7
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-07 09:33:19 +0200

    it parsed the test input lol

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+const std::string GREEN = "\033[32m";
+const std::string RED = "\033[31m";
+const std::string RESET = "\033[0m";
 
 int main() {
 
@@ -25,11 +28,16 @@ int main() {
       cout << "Remaining stream : [" << stream << "]\n" << endl;
     }
   } catch (std::runtime_error e) {
-    cout << e.what() << endl;
+    cout << RED << e.what() << RESET << endl;
+    cout << RED << "COMPILING FAILED LEXER STAGE:" << RESET << endl;
   }
 
+  // ------------ MEMORY MANAGEMENT ------------
+
   for (auto token : tokenList) {
-    cout << "Token code :[" << token->getCode() << "]" << endl;
+    cout << GREEN << "Token code :[" << token->getCode() << "]" << RESET
+         << endl;
+    delete token;
   }
 
   delete chain;
